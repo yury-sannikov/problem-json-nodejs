1808c9a45b4b31b5f7f57928507282f6e0324060
Yuriy Sanikov <yxs0@nreca.org>
2014-04-21 09:45:45 -0400

    Implemented: Naive implementation of problem+json

--- test/express-problem-json.test.js
+++ test/express-problem-json.test.js
@@ -8,22 +8,59 @@ var request = require('supertest')
 
 var app = express(); 
 
-app.use(problemJson());
-
 
 describe('normal express calls', function(){
   var app = express();
-  app.use(problemJson());
 
   app.get('/', function(req, res){
     res.send(200, {success: true});
   });
 
   var agent = request.agent(app);
+  app.use(problemJson());
 
   it('should get success', function(done){
     agent
     .get('/')
     .expect({success: true}, done);
   })
+})
+
+describe('execution fault during call', function(){
+
+	it('exception should return problem+json', function(done){
+		var app = express();
+
+		app.get('/', function(req, res, next){
+			throw "exception thrown";
+		});
+
+		var agent = request.agent(app);
+		app.use(problemJson());
+		
+		agent.superagent.parse['application/problem+json'] = agent.superagent.parse['application/json'];
+
+		agent.get('/')
+			.expect(500)
+			.expect('Content-Type', 'application/problem+json')
+			.expect({detail: "exception thrown", status: 500, title: "An error occured", type: "about:blank"}, done);
+	})
+
+	it('next with err should return problem+json', function(done){
+		var app = express();
+
+		app.get('/', function(req, res, next){
+			next("next return error");
+		});
+
+		var agent = request.agent(app);
+		app.use(problemJson());
+		
+		agent.superagent.parse['application/problem+json'] = agent.superagent.parse['application/json'];
+
+		agent.get('/')
+			.expect(500)
+			.expect('Content-Type', 'application/problem+json')
+			.expect({detail: "next return error", status: 500, title: "An error occured", type: "about:blank"}, done);
+	})
 })
